00696c6d25fc8788f7523b23198828dbdd4e1b9b
Samuel Adetunji <guysnacho2@gmail.com>
2020-05-23 18:28:24 -0500

    Styled the submit button

--- src/components/Submit.vue
+++ src/components/Submit.vue
@@ -6,4 +6,17 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import url(./style/Buttons.scss);
+
+#submit {
+  background-color: #b85c5a;
+  color: #f5f2f5;
+  border-radius: 25px 25px 25px 25px;
+  width: 50%;
+  height: 3em;
+  box-shadow: 0.5em·0 0.5em·0 6px #00000062;
+
+  margin-top: 4em;
+}
+</style>
