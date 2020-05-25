e86c06717d63d28d306f626564eb274c0a44cd5e
Samuel Adetunji <guysnacho2@gmail.com>
2020-05-25 13:08:37 -0500

    Cached the theme, re added router view

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,25 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import minifyTheme from "minify-css-string";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    options: {
+      minifyTheme,
+      themeCache: {
+        get: key => localStorage.getItem(key),
+        set: (key, value) => localStorage.setItem(key, value)
+      }
+    },
+    themes: {
+      light: {
+        primary: "#27AECB",
+        secondary: "#F5F2F5",
+        accent: "#C2A459",
+        error: "#B85C5A"
+      }
+    }
+  }
+});
