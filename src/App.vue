def2f2a0eb950d9104cfe76b1d986ad0ce2c4e7a
Samuel Adetunji <guysnacho2@gmail.com>
2020-05-25 11:58:08 -0500

    Added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,54 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: "app",
+import HelloWorld from "./components/HelloWorld";
 
-  data() {
-    return {};
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld
   },
 
-  methods: {},
-
-  components: {},
-
-  created() {
-    this.$router.push("/");
-  }
+  data: () => ({})
 };
 </script>
-
-<style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
-
-body {
-  text-decoration: none;
-  background-color: #f5f2f5;
-  font-family: "Jost", Helvetica, Arial, sans-serif;
-}
-
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
---
+++ src/Original.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+
+  components: {},
+
+  created() {
+    this.$router.push("/");
+  }
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
+
+body {
+  text-decoration: none;
+  background-color: #f5f2f5;
+  font-family: "Jost", Helvetica, Arial, sans-serif;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
