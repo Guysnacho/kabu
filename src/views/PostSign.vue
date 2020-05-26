adcd0866f4a60b8b45d90c50511463e49b30a5c4
Samuel Adetunji <guysnacho2@gmail.com>
2020-05-26 15:07:01 -0500

    Finished confirmation page

--- src/views/PostSign.vue
+++ src/views/PostSign.vue
@@ -1,11 +1,30 @@
 <template>
   <v-app>
-    <v-content> </v-content>
+    <v-content>
+      <v-col>
+        <v-row justify="center">
+          <v-card
+            color="primary"
+            dark
+            elevation="10"
+            min-height="20%"
+            max-height="25%"
+            max-width="79%"
+            shaped
+          >
+            <v-card-title>Thank you for joining the Kabu family!</v-card-title>
+            <v-card-text>
+              Please check your email for a confirmation link, then you'll be
+              free to make rides. You can return to the homepage with the button
+              above.
+            </v-card-text>
+          </v-card>
+        </v-row>
+      </v-col>
+    </v-content>
   </v-app>
 </template>
 
 <script>
 export default {};
 </script>
-
-<style></style>
