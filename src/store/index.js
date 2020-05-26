837e81b0b22ebd674a6d3f4c1cab82fc3d377751
Samuel Adetunji <guysnacho2@gmail.com>
2020-05-26 10:24:46 -0500

    Setup user properties

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,70 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import { firestorePlugin } from "vuefire";
+import * as firebase from "firebase/app";
 
-Vue.use(Vuex);
-Vue.use(firestorePlugin);
+Vue.use(Vuex, firestorePlugin);
 
 export default new Vuex.Store({
   state: {
-    loadingStatus: "notLoading",
-    user: {
-      id: "",
-      address: ""
+    localuser: "",
+    props: {
+      name: "",
+      email: "",
+      uid: ""
     }
   },
 
   mutations: {
-    /* LOGIN = (state) => { state.goLogin = false ;},
-    GOSIGN = (state) => { state.goSign = true; } */
-  },
-
-  actions: {
-    goToLogin: context => {
-      context.commit("GOLOGIN");
+    LOGIN(email, password) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode + errorMessage);
+        });
     },
-    goToSignUp: context => {
-      context.commit("GOLOGIN");
+    SIGN(email, password) {
+      this.state.props.email = email;
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode + errorMessage);
+          return error;
+        });
+    },
+    CHANGESTATE(state) {
+      firebase.auth().onAuthStateChanged(function(user) {
+        state.localuser = firebase.auth().currentUser;
+        if (user) {
+          // User is signed in.
+          state.props.name = state.user.name;
+          state.props.email = state.user.email;
+          state.props.uid = state.user.uid;
+        } else {
+          // No user is signed in.
+          console.log("User not signed in");
+        }
+      });
     }
   },
 
-  modules: {}
+  actions: {
+    loginToApp: context => {
+      context.commit("LOGIN, ");
+    },
+    signUp: context => {
+      context.commit("SIGN");
+    },
+    checkUser: context => {
+      context.commit("CHANGESTATE, state");
+    }
+  }
 });
