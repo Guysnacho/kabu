43904c78ad46421d65fad0687de2b6d239c33961
Samuel Adetunji <guysnacho2@gmail.com>
2020-05-27 14:40:19 -0500

    Moved stuff to the Home page

--- src/App.vue
+++ src/App.vue
@@ -28,28 +28,6 @@
       </v-btn>
     </v-app-bar>
 
-    <v-app-bar
-      v-else
-      app
-      dark
-      elevation="10"
-      color="primary"
-      src="https://cdn.pixabay.com/photo/2019/04/18/07/51/dalat-4136346_960_720.jpg"
-      fixed
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(44,40,65,.20), rgba(12,1,76,.60)"
-          cover
-          position="top"
-        ></v-img>
-      </template>
-
-      <v-app-bar-nav-icon dark></v-app-bar-nav-icon>
-      <v-toolbar-title>Kabu</v-toolbar-title>
-    </v-app-bar>
-
     <v-content>
       <router-view></router-view>
     </v-content>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
   <v-app>
+    <v-app-bar
+      app
+      dark
+      elevation="10"
+      color="primary"
+      src="https://cdn.pixabay.com/photo/2019/04/18/07/51/dalat-4136346_960_720.jpg"
+      fixed
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(44,40,65,.20), rgba(12,1,76,.60)"
+          cover
+          position="top"
+        ></v-img>
+      </template>
+      <v-app-bar-nav-icon dark></v-app-bar-nav-icon>
+      <v-row justify="center">
+        <v-toolbar-title>Username</v-toolbar-title>
+      </v-row>
+    </v-app-bar>
+
     <v-content>
+      <v-container></v-container>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, libero
         dolore. Ullam fugit vitae totam dolores, voluptates impedit suscipit
